2090bb2632132567a2ba34fcee717fbbaad498dd
ssuniie <phatsanphon2004@outlook.com>
2022-09-16 12:00:46 +0700

    solved week 8 problem, 1 in total.

--- wk8 - Inflation (U).py
+++ wk8 - Inflation (U).py
@@ -10,7 +10,7 @@ def main():
     price = float(input())
     years = int(input())
 
-    price_with_inflation = price * (1 + INFLATION_RATE*years)
+    price_with_inflation = price * (1 + INFLATION_RATE)**years
 
     price_with_inflation = int(price_with_inflation * 100) / 100
 
---
+++ wk8 - RuleofThree (U).py
@@ -0,0 +1,35 @@
+'''Week 8 - RuleOfThree'''
+
+
+def main():
+    '''Main Function'''
+
+    total_pieces = int(input())
+
+    best_snack_price = 0
+    best_snack_weight = 0
+
+    best_price_per_weight = 0
+
+    for _ in range(total_pieces):
+        price = int(input())
+        weight = float(input())
+
+        price_per_weight = price / weight
+
+        if price_per_weight > best_price_per_weight:
+            best_price_per_weight = price_per_weight
+
+            best_snack_price = price
+            best_snack_weight = weight
+        elif price_per_weight == best_price_per_weight:
+            if price < best_snack_price:
+                best_price_per_weight = price_per_weight
+
+                best_snack_price = price
+                best_snack_weight = weight
+
+    print('%.2f %.2f' % (best_snack_price, best_snack_weight))
+
+
+main()
