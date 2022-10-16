b1bb4dfdc3a323eb9aec435b2ff51eb79a7f949d
ssuniie <phatsanphon2004@outlook.com>
2022-10-16 22:55:07 +0700

    solved week 12 problems, 6 in total.

--- wk10m3 - CalculatorV2.py
+++ wk10m3 - CalculatorV2.py
@@ -17,24 +17,5 @@ def main():
         print(1)
         return
 
-    if len(n_value) < 2:
-        print(int(n_value) * 2)
-        return
-
-    total_press = 9 * 2
-
-    for i in range(2, n_value_length+2):
-        decrement = int('9'*(i-1)) - 10 * (i-1)
-
-        multiplier = int(n_value) - decrement
-
-        print(decrement, multiplier, n_value)
-
-        total_press += multiplier * (i + 1 + 1)
-
-        print('total press', total_press)
-
-    print(total_press)
-
 
 main()
